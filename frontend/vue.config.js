7cbab3e2dca96349a2a9891cc40ffec297ebebe9
bangtx <bangtx@otani-trading.com>
2023-04-20 16:08:55 +0700

    add

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,5 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  chainWebpack: config => {
+    config
+    .plugin('html')
+    .tap(args => {
+      args[0].title = 'Auto Xuân Phúc'
+      return args
+    })
+  }
 })
