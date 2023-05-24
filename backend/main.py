4ad834e9d6878c645cc1c2de6a4283cc6dc713da
bangtx <bangtx@otani-trading.com>
2023-05-24 22:02:37 +0700

    add meta

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from config.database import db
 from router import api
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 
@@ -24,6 +25,22 @@ def startup():
 def startup():
     if not db.is_closed():
         db.close()
+
+
+@app.get("/items/", response_class=HTMLResponse)
+async def read_items():
+    print('test')
+    return """
+    <html>
+        <head>
+            <meta name="zalo-platform-site-verification" content="EDZbEFZNSM87t_PktCrWULxXiN2ods4kCZW" />
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
 
 
 app.include_router(api.router)
